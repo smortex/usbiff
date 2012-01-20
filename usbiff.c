4f37a1940541ab357cdabe4dcc20fd9aa189065b
Romain Tartière <romain@blogreen.org>
2012-01-20 21:59:16 +0100

    Remove unused headers.

--- usbiff.c
+++ usbiff.c
@@ -27,11 +27,8 @@
 #include <sys/types.h>
 
 #include <sys/event.h>
-#include <sys/stat.h>
-#include <sys/time.h>
 
 #include <err.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
